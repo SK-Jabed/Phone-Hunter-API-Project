54e0d31f3e87da0468cc518e0199f02bcde085c5
Sheikh Jabed <jabedbd2295@gmail.com>
2024-10-04 23:57:40 +0600

    Main JS File Updated

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
+// Functionality of Load All Phones
 const loadAllPhones = () => {
     // Hide the spinner after 2 seconds
     const loadingSpinner = document.getElementById("spinner");
     loadingSpinner.style.display = "none";
 };
 
-
+// Functionality of Search Button
 const handleSearchButton = () => {
     // Show the spinner
     const loadingSpinner = document.getElementById("spinner");
